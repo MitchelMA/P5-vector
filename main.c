8d832a828b7774d4c53a9f8742c1c00b9f9250f6
MitchelMA <32548@ma-web.nl>
2022-11-09 14:38:56 +0100

    gebruik van vector d om van A naar B te berekenen

--- main.c
+++ main.c
@@ -9,17 +9,27 @@ int main(void)
     // positions of objects A and B
     Vector2 *a, *b;
 
+    // Calculate from object A towards B
+    Vector2 *d;
+
     a = newVec2(-6, 3);
     b = newVec2(5, 1);
 
     printf("Vector a voor GameObject A: ");
     printVec2(a);
-    printf("\nVector b voor GameObject B: ");
+    printf("Vector b voor GameObject B: ");
     printVec2(b);
+
+    // calculating the vector with a as tail and b as head
+    d = newVec2(b->x, b->y);
+    vec2SubVector(d, a);
+    printf("\nVector d wijzend van A naar B: ");
+    printVec2(d);
 
 
 
     free(a);
     free(b);
+    free(d);
     return EXIT_SUCCESS;
 }
