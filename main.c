6601e6e1c690a8f0dfe96ea6e77d2ce26351411e
MitchelMA <32548@ma-web.nl>
2022-11-09 15:02:01 +0100

    opdrachten van 2.1

--- main.c
+++ main.c
@@ -13,9 +13,16 @@ int main(void)
     // Calculate from object A towards B
     Vector2 *d;
 
+    // use for temporary vectors
+    Vector2 *tmp;
+
     // linear formula l(x)
     Pqr *l;
 
+    // 2 De Basis
+
+    // 2.1 De Basisobjecten ----
+    printf("2.1: De Basis\n");
     a = newVec2(-6, 3);
     b = newVec2(5, 1);
 
@@ -24,15 +31,37 @@ int main(void)
     printf("Vector b voor GameObject B: ");
     printVec2(b);
 
-    // calculating the vector with a as tail and b as head
+    // opdracht 1 ---
     d = newVec2(b->x, b->y);
     vec2SubVector(d, a);
-    printf("\nVector d wijzend van A naar B: ");
+    printf("\n1. Bereken de verschilvector d = b - a\n");
+    printf("\tVector d wijzend van A naar B: ");
     printVec2(d);
+    // --------------
+
+    // opdracht 2 ---
+    printf("\n2. Bereken de lengte van de verschilvector ||d||\n");
+    printf("\tLengte van deze vector: %f\n", vec2Magnitude(d));
+    // -------------- 
+    
+    // opdracht 3 ---
+    printf("\n3. Bereken de genormeerde eenheidsvector van d\n");
+    tmp = vec2Normalized(d);
+    printf("\tgenormeerde vector hiervan: ");
+    printVec2(tmp);
+    free(tmp);
+    // --------------
+
+    // -------------------------
+
+    // 2.2 De enemy ------------
+
+    // -------------------------
+
 
     // calculate linear formula through A and B
     l = newPqrPoints(a, b);
-    printf("lineaire formule door A en B: ");
+    printf("\nlineaire formule door A en B: ");
     printPqr(l);
 
 
